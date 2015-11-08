732a8e45ce28b953ace7105aba8e3a97560f3b1a
Varun Ramesh <vramesh2@stanford.edu>
2015-11-07 23:37:13 -0800

    Use an Atomic file library so interrupted crawling does not result in corrupted images.

--- flickr_scrape.py
+++ flickr_scrape.py
@@ -2,30 +2,39 @@ import requests
 import string
 import os.path
 import shutil
+from atomicfile import AtomicFile
 
 photo_url= string.Template("https://farm${farmid}.staticflickr.com/${serverid}/${id}_${secret}.jpg")
+page = 1
 
-r = requests.get('https://api.flickr.com/services/rest/', params = {
-    'method': 'flickr.photos.search',
-    'format': 'json',
-    'nojsoncallback': 1,
-    'api_key': '0b34944b5b61b43ec4fb3dc6389377a6',
-    'tags': 'yellowstone,landscape',
-    'tag_mode': 'all'
-})
+while True:
+    r = requests.get('https://api.flickr.com/services/rest/', params = {
+        'method': 'flickr.photos.search',
+        'format': 'json',
+        'nojsoncallback': 1,
+        'api_key': '0b34944b5b61b43ec4fb3dc6389377a6',
+        'tags': 'yellowstone,landscape',
+        'tag_mode': 'all',
+        'page': page,
+    })
 
-for photo in r.json()['photos']['photo']:
-    file = "data/flickr/" + photo['id'] + '.jpg'
-    url = photo_url.substitute(farmid=photo['farm'], serverid=photo['server'], id=photo['id'], secret=photo['secret'])
-    if not os.path.isfile(file):
-        print "Downloading " + url + " to " + file + "..."
-        photo_req = requests.get(url)
-        if photo_req.status_code != requests.codes.ok:
-            print "Error requesting photo."
-            break
+    json = r.json()
+    for photo in json['photos']['photo']:
+        file = "data/flickr/" + photo['id'] + '.jpg'
+        url = photo_url.substitute(farmid=photo['farm'], serverid=photo['server'], id=photo['id'], secret=photo['secret'])
+        if not os.path.isfile(file):
+            print "Downloading " + url + " to " + file + "..."
+            photo_req = requests.get(url)
+            if photo_req.status_code != requests.codes.ok:
+                print "Error requesting photo."
+                break
 
-        with open(file, 'wb') as fd:
-            for chunk in photo_req.iter_content(1024):
-                fd.write(chunk)
-    else:
-        print file + " already exists..."
+            with AtomicFile(file, 'wb') as fd:
+                for chunk in photo_req.iter_content(1024):
+                    fd.write(chunk)
+        else:
+            print file + " already exists..."
+
+    if page < json['photos']['pages']:
+        print "Querying page", page, "out of", json['photos']['pages']
+        page += 1
