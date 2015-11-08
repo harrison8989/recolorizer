87e6e4ae14c3447585c4c2d662bdf8d9f6154bcb
Varun Ramesh <vramesh2@stanford.edu>
2015-11-07 23:46:32 -0800

    Added comments to scraping script, added Pillow to requirements.

--- flickr_scrape.py
+++ flickr_scrape.py
@@ -4,10 +4,13 @@ import os.path
 import shutil
 from atomicfile import AtomicFile
 
+# URL template for a Flickr image.
 photo_url= string.Template("https://farm${farmid}.staticflickr.com/${serverid}/${id}_${secret}.jpg")
+# Page counter.
 page = 1
 
 while True:
+    # Search for images with the desired tags.
     r = requests.get('https://api.flickr.com/services/rest/', params = {
         'method': 'flickr.photos.search',
         'format': 'json',
@@ -22,6 +25,8 @@ while True:
     for photo in json['photos']['photo']:
         file = "data/flickr/" + photo['id'] + '.jpg'
         url = photo_url.substitute(farmid=photo['farm'], serverid=photo['server'], id=photo['id'], secret=photo['secret'])
+
+        # Only try to create the file if it doesn't exist already.
         if not os.path.isfile(file):
             print "Downloading " + url + " to " + file + "..."
             photo_req = requests.get(url)
@@ -29,12 +34,14 @@ while True:
                 print "Error requesting photo."
                 break
 
+            # Write the file atomically, so we don't have corrupted images.
             with AtomicFile(file, 'wb') as fd:
                 for chunk in photo_req.iter_content(1024):
                     fd.write(chunk)
         else:
             print file + " already exists..."
 
+    # Continue until we've exhausted all of the pages.
     if page < json['photos']['pages']:
         print "Querying page", page, "out of", json['photos']['pages']
         page += 1
