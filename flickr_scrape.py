7b067a8a6ce2820d42ed65ec6eb6acd4835f20cf
Varun Ramesh <vramesh2@stanford.edu>
2015-11-07 22:44:32 -0800

    Basic flickr scraping.

---
+++ flickr_scrape.py
@@ -0,0 +1,27 @@
+import requests
+import string
+import os.path
+import shutil
+
+photo_url= string.Template("https://farm${farmid}.staticflickr.com/${serverid}/${id}_${secret}.jpg")
+
+r = requests.get('https://api.flickr.com/services/rest/', params = {
+    'method': 'flickr.photos.search',
+    'format': 'json',
+    'nojsoncallback': 1,
+    'api_key': '0b34944b5b61b43ec4fb3dc6389377a6',
+    'tags': 'yellowstone,landscape'
+})
+
+for photo in r.json()['photos']['photo']:
+    file = "data/flickr/" + photo['id'] + '.jpg'
+    url = photo_url.substitute(farmid=photo['farm'], serverid=photo['server'], id=photo['id'], secret=photo['secret'])
+    if not os.path.isfile(file):
+        print "Downloading " + url + " to " + file + "..."
+        photo_req = requests.get(url)
+        if photo_req.status_code != requests.codes.ok:
+            print "Error requesting photo."
+            break
+
+        with open(file, 'wb') as fd:
+            shutil.copyfileobj(photo_req.raw, fd)
